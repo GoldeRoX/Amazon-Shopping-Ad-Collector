4760fce30ba0247fec8b53448fce370f0c7873ed
krzysztof <k.czupryna@interia.pl>
2022-06-28 13:30:36 +0200

    refaktoryzacja ad_factory.py

--- Factory/base.py
+++ Factory/base.py
@@ -1,30 +1,24 @@
+import os
+
 from appium import webdriver  # import Appium-Python-Client 2.2.0
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
+import cv2  # import opencv-python	4.5.5.64
+from Factory.Ad import Ad
+
+from Factory.database_connector import get_last_saved_id_from_db
 
 
-class WebDriver(object):
-    __platformName = "Android"
-    __platformVersion = "9"
-    __automationName = "UiAutomator2"
-    __appPackage = "com.amazon.mShop.android.shopping"
-    __appActivity = "com.amazon.mShop.home.HomeActivity"
-    __deviceName = "emulator-5554"
-    __uiautomator2ServerLaunchTimeout = "40000"
-    __iosInstallPause = "8000"
-    __wdaStartupRetryInterval = "20000"
-    __newCommandTimeout = "20000"
-    __skipDeviceInitialization = "True"
-    __skipServerInstallation = "True"
-    __noReset = "True"
+class MyDriver(object):
 
-    def __init__(self, platform_name=__platformName, platform_version=__platformVersion,
-                 automation_name=__automationName, app_package=__appPackage, app_activity=__appActivity,
-                 device_name=__deviceName, uiautomator_2_server_launch_timeout=__uiautomator2ServerLaunchTimeout,
-                 ios_install_pause=__iosInstallPause, wda_startup_retry_interval=__wdaStartupRetryInterval,
-                 new_command_timeout=__newCommandTimeout, skip_device_initialization=__skipDeviceInitialization,
-                 skip_server_installation=__skipServerInstallation, no_reset=__noReset):
+    def __init__(self, platform_name="Android", platform_version="9",
+                 automation_name="UiAutomator2", app_package="com.amazon.mShop.android.shopping",
+                 app_activity="com.amazon.mShop.home.HomeActivity", device_name="emulator-5554",
+                 uiautomator_2_server_launch_timeout="40000", ios_install_pause="8000",
+                 wda_startup_retry_interval="20000", new_command_timeout="20000", skip_device_initialization="True",
+                 skip_server_installation="True", no_reset="True"):
         __desired_caps = {
             "platformName": platform_name,
             "appium:platformVersion": platform_version,
@@ -44,15 +38,49 @@ class WebDriver(object):
         self.driver = webdriver.Remote("http://localhost:4723/wd/hub", __desired_caps)
 
 
-class Base(object):
+def save_cropped_scr(driver, ad) -> None:
+    date_folder_name = datetime.now().strftime("%Y-%m-%d")
 
-    def send_text(self, by_type, path: str, text_to_send: str) -> None:
-        try:
-            WebDriverWait(self.driver, 5).until(
-                EC.presence_of_element_located((by_type, path)))
-            self.driver.find_element(by_type, path).send_keys(text_to_send)
-        except (NoSuchElementException, TimeoutException):
-            print("No such Input field")
+    if not os.path.exists(f"/nfsshare/Screenshots/{date_folder_name}"):
+        os.mkdir(f"/nfsshare/Screenshots/{date_folder_name}")
+
+    img_name = int(get_last_saved_id_from_db()) + 1
+
+    image_path = f"/nfsshare/Screenshots/{date_folder_name}/{str(img_name)}.png"
+    driver.save_screenshot(image_path)
+    img = cv2.imread(image_path)
+
+    """cropped_image = img[
+                    ad.location_y:ad.location_y + ad.height,
+                    ad.location_x:ad.location_x + ad.width]"""
+
+    cropped_image = img[
+                    ad.location["y"]:ad.location["y"] + ad.size["height"],
+                    ad.location["x"]:ad.location["x"] + ad.size["width"]]
+    cv2.imwrite(image_path, cropped_image)
 
 
-driver = WebDriver().driver
+def click_element(driver, by_type, path: str, time_to_wait=5) -> None:
+    try:
+        WebDriverWait(driver, time_to_wait).until(
+            EC.presence_of_element_located((by_type, path)))
+        driver.find_element(by_type, path).click()
+    except (NoSuchElementException, TimeoutException):
+        pass
+
+
+def send_text(driver, by_type, path: str, text_to_send: str) -> None:
+    try:
+        WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((by_type, path)))
+        driver.find_element(by_type, path).send_keys(text_to_send)
+    except (NoSuchElementException, TimeoutException):
+        print("No such Input field")
+
+
+"""def first_launch(self) -> None:
+    time.sleep(3)
+    if self.driver.find_element(By.ID, "com.amazon.mShop.android.shopping:id/btn_cancel").is_displayed():
+        Search.click_element(self, By.ID, "com.amazon.mShop.android.shopping:id/btn_cancel")
+    if self.driver.find_element(By.ID, "com.amazon.mShop.android.shopping:id/skip_sign_in_button").is_displayed():
+        Search.click_element(self, By.ID, "com.amazon.mShop.android.shopping:id/skip_sign_in_button")"""
--- Factory/main.py
+++ Factory/main.py
@@ -1,179 +1,102 @@
-import os.path
 import random
 import time
-from datetime import datetime
-import cv2  # import opencv-python	4.5.5.64
 
-from threading import Lock
 from appium.webdriver.common.touch_action import TouchAction
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from appium import webdriver  # import Appium-Python-Client 2.2.0
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from appium.webdriver.webelement import WebElement
 
 from Factory.locators_data import LocatorsData
-from Factory.database_connector import get_last_saved_id_from_db
+from base import *
 
 from Ad import Ad
 
-from Factory.base import driver
 
-"""class WebDriverMeta(type):
-    _instances = {}
+def set_up(driver, phrase_to_search: str) -> None:
+    """search item"""
+    driver.find_element(By.XPATH, LocatorsData.search_icon).click()
+    #click_element(driver, By.XPATH, LocatorsData.search_icon)
+    send_text(driver, By.ID, LocatorsData.search_input, phrase_to_search)
 
-    _lock: Lock = Lock()
+    """press enter"""
+    driver.press_keycode(66)
 
-    def __call__(cls, *args, **kwargs):
-        with cls._lock:
-            if cls not in cls._instances:
-                instance = super().__call__(*args, **kwargs)
-                cls._instances[cls] = instance
-        return cls._instances[cls]
-
-
-class WebDriver(metaclass=WebDriverMeta):
-    # TODO dac parametry jako atrybuty osobne
-    def __init__(self):
-        __desired_caps = {
-            "platformName": "Android",
-            "appium:platformVersion": "9",
-            "appium:automationName": "UiAutomator2",
-            "appium:appPackage": "com.amazon.mShop.android.shopping",
-            "appium:appActivity": "com.amazon.mShop.home.HomeActivity",
-            "appium:deviceName": "emulator-5554",
-            "uiautomator2ServerLaunchTimeout": "40000",
-            "iosInstallPause": "8000",
-            "wdaStartupRetryInterval": "20000",
-            "newCommandTimeout": "20000",
-            "skipDeviceInitialization": "True",
-            "skipServerInstallation": "True",
-            "noReset": "True"
-        }
-
-        self.driver = webdriver.Remote("http://localhost:4723/wd/hub", __desired_caps)"""
-
-
-class Search(object):
-
-    driver = driver
-
-    def save_cropped_scr(self, object_to_save) -> None:
-        date_folder_name = datetime.now().strftime("%Y-%m-%d")
-
-        if not os.path.exists(f"../Screenshots/{date_folder_name}"):
-            os.mkdir(f"../Screenshots/{date_folder_name}")
-
-        img_name = int(get_last_saved_id_from_db()) + 1
-
-        image_path = f"../Screenshots/{date_folder_name}/{str(img_name)}.png"
-        self.driver.save_screenshot(image_path)
-        img = cv2.imread(image_path)
-
-        cropped_image = img[
-                        object_to_save.location["y"]:object_to_save.location["y"] + object_to_save.size[
-                            "height"],
-                        object_to_save.location["x"]:object_to_save.location["x"] + object_to_save.size[
-                            "width"]]
-        cv2.imwrite(image_path, cropped_image)
-
-    def click_element(self, by_type, path: str, time_to_wait=5) -> None:
+    """scroll through app Y"""
+    for i in range(16):
         try:
-            WebDriverWait(self.driver, time_to_wait).until(
-                EC.presence_of_element_located((by_type, path)))
-            self.driver.find_element(by_type, path).click()
-        except (NoSuchElementException, TimeoutException):
+            driver.swipe(470, 1100, 470, 50, 400)
+            time.sleep(1)
+        except:
             pass
 
-    def send_text(self, by_type, path: str, text_to_send: str) -> None:
-        try:
-            WebDriverWait(self.driver, 5).until(
-                EC.presence_of_element_located((by_type, path)))
-            self.driver.find_element(by_type, path).send_keys(text_to_send)
-        except (NoSuchElementException, TimeoutException):
-            print("No such Input field")
 
-    """def first_launch(self) -> None:
-        time.sleep(3)
-        if self.driver.find_element(By.ID, "com.amazon.mShop.android.shopping:id/btn_cancel").is_displayed():
-            Search.click_element(self, By.ID, "com.amazon.mShop.android.shopping:id/btn_cancel")
-        if self.driver.find_element(By.ID, "com.amazon.mShop.android.shopping:id/skip_sign_in_button").is_displayed():
-            Search.click_element(self, By.ID, "com.amazon.mShop.android.shopping:id/skip_sign_in_button")"""
+def bottom_ad(driver) -> None:
+    try:
+        element = driver.find_element(By.XPATH, LocatorsData.BOTTOM_AD)
 
-    def set_up(self, phrase_to_search: str) -> None:
-        """search item"""
-        Search.click_element(self, By.XPATH, LocatorsData.search_icon)
+        if element.size["height"] > 100 and element.get_attribute("scrollable") == "true":
+            """create an object of ad"""
+            ad = Ad(element, 1)
+            save_cropped_scr(driver, element)
+            ad.send_to_db()
 
-        Search.send_text(self, By.ID, LocatorsData.search_input, phrase_to_search)
+    except NoSuchElementException:
+        pass
 
-        """press enter"""
-        self.driver.press_keycode(66)
 
-        """scroll through app Y"""
-        for i in range(16):
+def get_ads_2(driver):
+    element_node = driver.find_element(By.XPATH, LocatorsData.brands_related_to_your_search_element_node)
+    elements = element_node.find_elements(By.XPATH, ".//*[@class='android.view.View']")
+    ads = []
+    for x in range(len(elements)):
+        if elements[x].get_attribute("clickable") == "true" and elements[x].get_attribute("text").startswith(
+                "Sponsored ad from"):
+            ads.append(elements[x])
+    return ads
+
+
+def execute_ad_2(driver) -> None:
+    try:
+        ads = get_ads_2(driver)
+
+        for x in range(len(ads)):
+            element = ads[x]
             try:
-                self.driver.swipe(470, 1100, 470, 50, 400)
-                time.sleep(1)
-            except:
-                pass
-
-    def bottom_ad(self) -> None:
-
-        try:
-            element = self.driver.find_element(By.XPATH, LocatorsData.BOTTOM_AD)
-
-            if element.size["height"] > 100 and element.get_attribute("scrollable") == "true":
                 """create an object of ad"""
-                ad = Ad(element, 1)
-                self.save_cropped_scr(element)
+                ad = Ad(element, 2)
+
+                save_cropped_scr(driver, element)
                 ad.send_to_db()
+                # TODO remake scroll
+                """scroll through ads"""
 
-        except NoSuchElementException:
-            pass
+                """first_ad = element
+                print(f" z {x} = {element}")
+                next_ad = ads[x+1]
+                print(f"do {x+1} = {ads[x+1]}")
+                driver.scroll(first_ad, next_ad)"""
 
-    def execute_ad_2(self) -> None:
-        try:
-            element_node = self.driver.find_element(By.XPATH, LocatorsData.brands_related_to_your_search_element_node)
-            elements = element_node.find_elements(By.XPATH, ".//*[@class='android.view.View']")
-            ads = []
-            for x in range(len(elements)):
-                if elements[x].get_attribute("clickable") == "true" and elements[x].get_attribute("text").startswith("Sponsored ad from"):
-                    ads.append(elements[x])
+                """scroll through ads"""
+                action = TouchAction(driver)
+                action.press(element).move_to(x=-element.size["width"] / 2, y=0).release().perform()
 
-            for x in range(len(ads)):
-                element = ads[x]
-                try:
-                    """create an object of ad"""
-                    ad = Ad(element, 2)
+            except Exception as e:
+                print(f'Exception occurred : {e}')
 
-                    self.save_cropped_scr(element)
-                    ad.send_to_db()
-                    # TODO zmodyfikowac tym sposobem scroll
-                    """scroll through ads"""
-                    """first_ad = ads[x]
-                    print(f" z {x} = {element}")
-                    next_ad = ads[x+1]
-                    print(f"do {x+1} = {ads[x+1]}")
-                    self.driver.scroll(first_ad, next_ad)"""
-
-                    """scroll through ads"""
-                    action = TouchAction(self.driver)
-                    action.press(element).move_to(x=-element.size["width"] / 2, y=0).release().perform()
-
-                except Exception as e:
-                    print(f'Exception occurred : {e}')
-
-        except (NoSuchElementException, TimeoutException):
-            pass
+    except (NoSuchElementException, TimeoutException):
+        pass
 
 
 if __name__ == "__main__":
+    session = MyDriver()
+
+    _driver = session.driver
     while True:
         try:
-            Amazon = Search()
             list_of_brands = ["Oculus", "Hp", "Laptops", "Monitors"]
-            Amazon.set_up(list_of_brands[random.randint(0, len(list_of_brands) - 1)])
-            Amazon.bottom_ad()
-            Amazon.execute_ad_2()
-        except Exception as e:
-            print(e)
+            set_up(_driver, list_of_brands[random.randint(0, len(list_of_brands) - 1)])
+            bottom_ad(_driver)
+            execute_ad_2(_driver)
+        except:
+            _driver.close()
+        """finally:
+            _driver.close()"""
