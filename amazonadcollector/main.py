ad4d6f02b75ace80f350d326cc259ab680d83c79
krzysztof <k.czupryna@interia.pl>
2023-06-01 14:13:44 +0200

    created create_and_save_ads() method that creates ad objects from dict. Key of dict is web_element of an ad and the value is ad_type of this ad. Simple match method creates correct ads corresponding to the ad_type.

--- amazonadcollector/main.py
+++ amazonadcollector/main.py
@@ -35,7 +35,7 @@ def main(udid: int):
     base_methods.first_launch()
     base_methods.cookies_click()
 
-    # base_methods.change_lang_from_eng_to_de()
+    base_methods.change_setting_to_de()
     # base_methods.change_setting_to_uk()
 
     session_id = SQLAdManager().get_last_saved_session_id_from_db() + 1
