8a342e0f83cd6fd7bde3eca8581afdac2ff3a459
krzysztof <k.czupryna@interia.pl>
2023-09-19 13:32:26 +0200

    ad top brand added

--- emulators_script_starts/Amazon-5558.py
+++ emulators_script_starts/Amazon-5558.py
@@ -1,9 +1,8 @@
 from amazonadcollector.main import main
 
 if __name__ == "__main__":
-    while True:
-        try:
-            main(5558)
-        except Exception as e:
-            print(e)
-            pass
+    try:
+        main(5558)
+    except Exception as e:
+        print(e)
+        pass
